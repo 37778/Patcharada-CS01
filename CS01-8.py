a1c95095db6613af2dc780938069bfcd54793688
37778 <37778@virtual.prc.ac.th>
2021-07-23 09:31:14 +0700

    Create CS01-8.py

---
+++ CS01-8.py
@@ -0,0 +1,18 @@
+a=int(input())
+b=int(input())
+if a+b <= 80:
+    print("A")
+if a+b <= 80:
+    print("B+")
+if a+b <= 70:
+    print("B")
+if a+b <= 65:
+    print("C+")
+if a+b <= 60:
+    print("C")
+if a+b <= 55:
+    print("D+")
+if a+b <= 50:
+    print("D")
+else:
+    print("F")
