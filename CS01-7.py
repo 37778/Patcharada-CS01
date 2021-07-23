90a0c7c440f7cd635a624ed5e19c0fa3b9fec078
37778 <37778@virtual.prc.ac.th>
2021-07-23 09:40:43 +0700

    Update CS01-7.py

--- CS01-7.py
+++ CS01-7.py
@@ -1,7 +1,18 @@
-A=int(input())
-if A<=10:
-    print("ไม่ผ่าน")
-elif A<=20:
-    print("ปรับปรุง")
+a=int(input())
+b=int(input())
+if a+b <= 80:
+    print("A")
+if a+b <= 80:
+    print("B+")
+if a+b <= 70:
+    print("B")
+if a+b <= 65:
+    print("C+")
+if a+b <= 60:
+    print("C")
+if a+b <= 55:
+    print("D+")
+if a+b <= 50:
+    print("D")
 else:
-    print("ดีมาก")
+    print("F")
