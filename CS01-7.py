19e7523c53c5ac201337c818cc95bfbadff0fc83
37778 <37778@virtual.prc.ac.th>
2021-09-25 18:13:04 +0700

    Update CS01-7.py

--- CS01-7.py
+++ CS01-7.py
@@ -1,20 +1,7 @@
-a=int(input("work score: "))
-b=int(input("midterm :"))
-c=int(input("final :"))
-d=a+b+c
-if d >= 80:
-    print("A")
-elif d >= 75:
-    print("B+")
-elif d >= 70:
-    print("B")
-elif d >= 65:
-    print("C+")
-elif d >= 60:
-    print("C")  
-elif d >= 55:
-    print("D+")
-elif d >= 50:
-    print("D")
+A=int(input())
+if A<=10:
+    print("ไม่ผ่าน")
+elif A<=20:
+    print("ปรับปรุง")
 else:
-    print("F")
+    print("ดีมาก")
